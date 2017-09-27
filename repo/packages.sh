d1f987ec12792c1f8c096b137868a6252e3a8b44
atirit <aydin.tirit@gmail.com>
2017-09-26 20:36:26 -0700

    Added auto-updater script (fuck yeah), updated LotusExt to 1.1 (Discord)

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,1 +1,2 @@
-dpkg-deb -b -Zlzma com.atirit.lotusextension
+dpkg-deb -b -Zlzma packagesrc/com.atirit.lotusextension
+find $PWD -type f -print0 | xargs -0 mv -t $PWD/..
--- repo/update.sh
+++ repo/update.sh
@@ -1,3 +1,31 @@
+cd "/Users/atirit/Documents/Coding/atirit.github.io/repo/packagesrc"
+
+base_dir=$PWD
+for DIR in "$base_dir"/*/
+do
+  echo $DIR
+  cd "$DIR/DEBIAN"
+
+  NAMELN=$(grep Name ./control)
+  NAMESP="$(echo $NAMELN | sed 's/^[^:]*://g')"
+  echo "Name of current package:$NAMESP"
+
+  VERLN=`grep Version ./control`
+  VERSP="$(echo $VERLN | sed 's/^[^:]*://g')"
+  OLDVER=$(echo $VERSP | sed 's/^[^" "]*" "//g')
+  echo "Current version of$NAMESP: $OLDVER"
+  read -p "What's the new version of$NAMESP? " NEWVER
+  case $NEWVER in
+    [""]* ) NEWVER=$OLDVER
+  esac
+  echo "New version of$NAMESP: $NEWVER"
+  sed '/Version/s/.*/Version: '"$NEWVER"'/' ./control > ./control.bak
+  rm ./control
+  mv ./control.bak ./control
+  cd ../..
+done
+
+cd ..
 ./remove.sh
 ./packages.sh
 
